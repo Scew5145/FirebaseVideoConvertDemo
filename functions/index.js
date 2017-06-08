a459a6762b0337ba76df76ad8a5366e7cf04874d
Scew5145 <scew5145@colorado.edu>
2017-06-08 17:01:47 -0600

    filepath parsing and video type checking

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,35 @@ const exec = require('child-process-promise').exec;
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_static = require('ffmpeg-static');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.storage.object().onChange(event => {
-  console.log("Hello World!");
+const MP4_EXTENSION = 'mp4';
+const TEMP_LOCAL_FOLDER = '/tmp/';
+
+
+
+/*
+*
+*/
+exports.webm-to-mp4 = functions.storage.object().onChange(event => {
+  const object = event.data;
+  const filePath = object.name;
+  const filePathSplit = filePath.split('/');
+  const fileName = filePathSplit.pop();
+  const baseFileName = fileName.split('.').slice(0,-1).join('.')
+  const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
+
+  const MP4FilePath = `${fileDir}${baseFileName}.${MP4_EXTENSION}`;
+  const tempLocalFile = `${tempLocalDir}${fileName}`;
+  const tempLocalJPEGFile = `${TEMP_LOCAL_FOLDER}${MP4FilePath}`;
+
+  if(!object.contentType.startsWith('video/')){
+    console.log('Item ', fileName, 'is not a video.')
+    return
+  }
+  if(object.contentType.startsWith('video/mp4')){
+    console.log('Item ', filename, 'Is already an mp4, no need to convert.')
+    return
+  }
+
+
+  console.log("Time to convert!");
 });
